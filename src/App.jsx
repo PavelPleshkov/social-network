97e56f96fbe3ee3c4f6b9f83893655b6a03f3438
Pavel <pleshkov.p@gmail.com>
2025-02-15 20:53:51 +0300

    feat: add component Dialogs with Dialogs.module.css, add links for routing to Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import './App.css';
 import Header from './Components/Header/Header'
 import Navbar from './Components/Navbar/Navbar'
 import Profile from './Components/Profile/Profile';
+import Dialogs from './Components/Dialogs/Dialogs';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Header />
       <Navbar />
-      <Profile />
+      <div className='App-content'>
+        {/* <Profile /> */}
+        <Dialogs />
+      </div>
+
     </div>
   );
 }
